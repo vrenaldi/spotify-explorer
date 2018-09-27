6e5ad7f72a9ee779eb52cafcc6383b8ebd5b032e
vrenaldi <vrenaldi258@gmail.com>
2018-09-27 13:15:19 +0200

    Implement about.

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    MaterialModule
   ],
   declarations: [AboutComponent]
 })
